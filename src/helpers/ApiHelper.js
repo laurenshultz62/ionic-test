8fe4e1ece5c7d73366f6584f409008be282822c9
Lauren Shultz <larz2016@gmail.com>
2020-01-30 17:00:09 -0600

    updated url back

--- src/helpers/ApiHelper.js
+++ src/helpers/ApiHelper.js
@@ -1,5 +1,4 @@
-// const baseEndpoint = "https://hello-world-native-script.herokuapp.com/"
-const baseEndpoint = "http://localhost:3000/"
+const baseEndpoint = "https://hello-world-native-script.herokuapp.com/"
 export const ApiHelper = {
     AUTHENTICATE_SIGNIN: () => baseEndpoint + "authenticate"
 }
